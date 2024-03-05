7d702e2512f8d3751397f6e7f3ae2869a33d3a97
Pratham Gupta <pratham.gupta2k@gmail.com>
2024-03-05 15:59:46 -0500

    Added Login Logout functionality

--- cineverse/accounts/views.py
+++ cineverse/accounts/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from .forms import RegisterForm, LoginForm
 
-# Create your views here.
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = RegisterForm()
+    return render(request, "accounts/register.html", {"form": form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("home")
+    else:
+        form = LoginForm()
+    return render(request, "accounts/login.html", {"form": form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
