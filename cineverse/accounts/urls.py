d311604516c57aa72eb1c56808f06ebf159511b1
Pratham Gupta <pratham.gupta2k@gmail.com>
2024-03-18 00:16:25 -0400

    Added UI Pages

--- cineverse/accounts/urls.py
+++ cineverse/accounts/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("register/", views.register, name="register"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
+    path("signin/", views.signin, name="signin"),
+    path("signup/", views.signup, name="signup"),
 ]
--- cineverse/accounts/views.py
+++ cineverse/accounts/views.py
@@ -30,3 +30,28 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('home')
+
+def signin(request):
+    if request.method == "POST":
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("home")
+    else:
+        form = LoginForm()
+    return render(request, "accounts/signin.html", {"form": form})
+
+def signup(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = RegisterForm()
+    return render(request, "accounts/signup.html", {"form": form})
